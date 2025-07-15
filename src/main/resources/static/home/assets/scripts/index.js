9182a2997dad8ce985597f27fff4f979f6749a26
taeyong <taeyong.k0902@gmail.com>
2025-07-15 17:35:18 +0900

    feat: display pins on home with hover actions and layout refinements

--- src/main/resources/static/home/assets/scripts/index.js
+++ src/main/resources/static/home/assets/scripts/index.js
@@ -2,4 +2,11 @@ import MasonryInit from '../../../assets/scripts/libraries/masonry.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     MasonryInit();
+
+    const $saveButton = document.querySelector('.overlay .save-button');
+
+    $saveButton.addEventListener('click', () => {
+        $saveButton.classList.add('active');
+        $saveButton.textContent = "저장됨";
+    });
 });
